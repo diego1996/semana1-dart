43c8ddec8c1c39f21144b6ff53bc166b81ffbc7d
Diego Alejandro Asencio Cuellar <diegoasencio96@gmail.com>
2021-09-04 07:57:30 -0500

    add example in class (variables in dart)

---
+++ 1.variables/variables.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  var firstName = "Diego";
+  String lastName = "Alejandro";
+
+  var age = 24;
+  int age2 = 24;
+
+  var test;
+
+  print(test);
+
+  test = "test";
+  print(test);
+
+  test = 0.5;
+  print(test);
+
+  print(firstName);
+  print(lastName);
+
+  print(age);
+  print(age2);
+
+  print("${firstName.toUpperCase()} ${lastName.toUpperCase()}");
+}
